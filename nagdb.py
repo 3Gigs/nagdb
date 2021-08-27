c67a2a08a3ae0e6218e57663d67496a654594b1f
Huaxuan Yang <yanghuaxuan@gmail.com>
2021-08-26 19:17:26 -0700

    Added hot reloading

--- nagdb.py
+++ nagdb.py
@@ -1,9 +1,16 @@
 from dotenv import load_dotenv
 import os
 import discord
+import logging
 from discord.ext import commands
 
 load_dotenv()
+
+logger = logging.getLogger("discord")
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename="discord.log", encoding="utf-8", mode="w")
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 bot = commands.Bot(command_prefix='?> ')
 
@@ -11,6 +18,17 @@ extensions = os.listdir("./Cogs")
 for xd in extensions:
     if xd.endswith(".py"):
         bot.load_extension(f"Cogs.{xd[:-3]}")
+
+"""Function to hot reload cogs"""
+@commands.command(name="reload")
+async def reload(ctx):
+    for xd in extensions:
+        if xd.endswith(".py"):
+            bot.reload_extension(f"Cogs.{xd[:-3]}")
+    await ctx.send("Cogs reloaded")
+
+bot.add_command(reload) 
+
 
 @bot.event
 async def on_ready():
