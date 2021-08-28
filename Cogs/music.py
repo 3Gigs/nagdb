6be634d0d5e0db3c1141add37755dd39bb9d64ba
Huaxuan Yang <yanghuaxuan@gmail.com>
2021-08-27 19:41:09 -0700

    Add type hints, google style docstrings, other minor changes

--- Cogs/music.py
+++ Cogs/music.py
@@ -1,14 +1,23 @@
-import discord
+# For type hinting reasons
+from discord import VoiceClient
+from discord import VoiceChannel
 from discord.ext import commands
 
-
 class MusicPlayer(commands.Cog, name="Music Player"):
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self._bot = bot
     
+    """Decides what to do from args string provided
+    Args: 
+        args: An argument from a discord command
+        userVC: Discord VoiceClient returned by VoiceChannel.connect()
+
+    Returns:
+        void
+    """
     async def parseArgs(args: str, userVC: VoiceClient):
-        userVC.play(discord.FFmpegOpusAudio(pipe=True))
-    
+        userVC.play(discord.FFmpegOpusAudio())
+
     @commands.command(name="play")
     async def joinVC(self, ctx: commands.Context):
         if(not ctx.args):
@@ -16,8 +25,8 @@ class MusicPlayer(commands.Cog, name="Music Player"):
         """Join Voice Channel"""
         channel: VoiceChannel = ctx.author.voice.channel
         vc: VoiceClient = await channel.connect()
-        if(not vc.is_play()):
-            await parseArgs(ctx.args, vc)
+        if(not vc.is_playing()):
+            await self.parseArgs(ctx.args, vc)
         
 
 def setup(_bot):
