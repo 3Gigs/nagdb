74eb24b65a4869e22511335f87250e9a8f6d7917
Huaxuan Yang <yanghuaxuan@gmail.com>
2021-12-16 22:14:55 -0800

    lmaoooooo

--- src/nagdb.ts
+++ src/nagdb.ts
@@ -1,13 +1,11 @@
 import { Client, Intents } from "discord.js";
 import { token } from "../config.json";
 import { commandHandler } from "./commandHandler";
-import { nagLogger } from "./modules/nagLogger";
 
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS,
     Intents.FLAGS.GUILD_VOICE_STATES] });
 
-nagLogger.getInstance().logBot(client);
 const cmdHandler = commandHandler.construct(client);
 
 client.once("ready", () => {
